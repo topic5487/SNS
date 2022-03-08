4fa6e3b16be611b2eb237d7dff1f2a0b0d027b05
[topic5487] <[topic5487@gmail.com]>
2022-03-08 14:48:03 +0800

    處理頁面頭尾View

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank class="text-decoration-none">
+    刻意練習，每日精進
+  </a>
+
+  <div class="float-end">
+    <a href="/about" class="text-decoration-none">關於</a>
+  </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,19 +5,13 @@
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">SNS</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">幫助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登入</a></li>
-        </ul>
-      </div>
-    </nav>
+    @include('layouts._header')
 
     <div class="container">
+      <div class="offset-md-1 col-md-10">
       @yield('content')
+      @include('layouts._footer')
+      </div>
     </div>
   </body>
 </html>
