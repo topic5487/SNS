4b7f2365f82e25a912dba97c8c74ddd73929d982
[topic5487] <[topic5487@gmail.com]>
2022-03-08 22:52:52 +0800

    註冊表單

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,16 +1,37 @@
 @extends('layouts.default')
+@section('title', '註冊')
 
 @section('content')
-<div class="bg-light p-3 p-sm-5 rounded">
-  <h1>主頁</h1>
-  <p class="lead">
-    你看到的是<a href="https://learnku.com/courses/laravel-essential-training">laravel範例主頁</a>
-  </p>
-  <p>
-    一切從這裡開始
-  </p>
-  <p>
-  <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">現在註冊</a>
-  </p>
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>註冊</h5>
+    </div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store') }}">
+          <div class="mb-3">
+            <label for="name">名稱：</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="mb-3">
+            <label for="email">E-mail：</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="mb-3">
+            <label for="password">密碼：</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="mb-3">
+            <label for="password_confirmation">確認密碼：</label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">註冊</button>
+      </form>
+    </div>
+  </div>
 </div>
 @stop
