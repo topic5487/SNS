83a3312fa55e1e3f7df2c1b5f9f7990c277f0684
[topic5487] <[topic5487@gmail.com]>
2022-03-10 17:32:03 +0800

    發佈文章

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Status;
+use Illuminate\Support\Facades\Auth;
+
+class StatusesController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request){
+        //先驗證發文內容
+        $this->validate($request, [
+            'content' => 'required|max:140'
+        ]);
+        //獲取當前用戶 並對發文內容賦值
+        Auth::user()->statuses()->create([
+            'content' => $request['content']
+        ]);
+        return redirect()->back();
+
+    }
+}
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,16 +1,32 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="bg-light p-3 p-sm-5 rounded">
-  <h1>主頁</h1>
-  <p class="lead">
-    你看到的是<a href="https://learnku.com/courses/laravel-essential-training">laravel範例主頁</a>
-  </p>
-  <p>
-    一切從這裡開始
-  </p>
-  <p>
-  <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">現在註冊</a>
-  </p>
-</div>
+  @if (Auth::check())
+  {{--已登入者 即顯示表單--}}
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="bg-light p-3 p-sm-5 rounded">
+      <h1>主頁</h1>
+      <p class="lead">
+        你現在看到的是 <a href="https://learnku.com/courses/laravel-essential-training"></a>。
+      </p>
+      <p>
+        一切，將從這裡開始。
+      </p>
+      <p>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">現在註冊</a>
+      </p>
+    </div>
+  @endif
 @stop
