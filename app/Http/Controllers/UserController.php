54a6f7772b518acf726c96187ee9e66134b0a904
[topic5487] <[topic5487@gmail.com]>
2022-03-10 13:06:56 +0800

    完善信件發送

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -89,12 +89,10 @@ class UserController extends Controller
     protected function sendEmailConfirmationTo($user){
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'hank@example.com';
-        $name = 'Hank';
         $to = $user->email;
         $subject = "感謝註冊！請確認您的信箱";
-        Mail::send($view, $data, function ($message) use($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
