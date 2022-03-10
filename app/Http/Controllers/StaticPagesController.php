2f3d407a682db4b1145cb4019f47874efc969c53
[topic5487] <[topic5487@gmail.com]>
2022-03-10 20:05:52 +0800

    首頁文章列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     public function home(){
-        return view('static_pages/home');
+        //定義一個空數組來存放貼文動態數據
+        $feed_items = [];
+        //確認用戶已登入
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help(){
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -8,6 +8,9 @@
         <section class="status_form">
           @include('shared._status_form')
         </section>
+        <h4>文章列表</h4>
+        <hr>
+        @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
