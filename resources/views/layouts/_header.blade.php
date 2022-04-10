f1fcc96a568aa564f476fa88e4224d8ee103a026
[topic5487] <[topic5487@gmail.com]>
2022-04-10 14:57:47 +0800

    Soft delete

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,7 +4,6 @@
     <ul class="navbar-nav justify-content-end">
       {{--判斷當前用戶是否已登錄--}}
       @if (Auth::check())
-      <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">用户列表</a></li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           {{ Auth::user()->name }}
@@ -12,6 +11,9 @@
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">個人中心</a>
           <a class="dropdown-item" href="{{ route('users.edit', Auth::user()) }}">編輯資料</a>
+            @if(Auth::user()->is_admin == 1)
+              <a class="dropdown-item" href="{{ route('users.index') }}">用户列表</a>
+            @endif
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" id="logout" href="#" >
             <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('確定要登出嗎?');">
